12f84ae37bb6aa5841bcd01e0291e24fa21667a4
SAWD ALABDALLAH <Soa142812222@gmail.com>
2021-02-22 16:18:38 +0300

    Add some changes and use sqlite instead of shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:budgetplanner/Models/expensesData.dart';
 import 'package:flutter/material.dart';
 import 'package:budgetplanner/Screens/MainScreen.dart';
-import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
